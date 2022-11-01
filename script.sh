7561688701519de8f0661529259b24d86e3aa5df
3ndrias <endriasargaw@gmail.com>
2022-11-01 13:53:42 +0300

    added regular expressions for each resolution

--- script.sh
+++ script.sh
@@ -5,3 +5,9 @@
 # 1. get the playlist
 page="$(wget -O - "$1")";
 
+# there are two types of m3u8 files, the first contain the urls for multiple playlists. Each playlist in
+# these playlists is an m3u8 file, each playlist will have an inline specified BW and resolution, the ff
+# regular expression chooses the 720p resolution playlist
+1080pRE='^(\#EXT-X-STREAM-INF:PROGRAM-ID=[0-9]+,BANDWIDTH=[0-9]+,RESOLUTION=(19[12][890123]x[0-9]+|[0-9]+x10[78][7890123]),FRAME-RATE=[0-9]+(\.[0-9]+)?,CODECS="[^"]+"\n)(https:\/\/.+)'
+720pRE='^(\#EXT-X-STREAM-INF:PROGRAM-ID=[0-9]+,BANDWIDTH=[0-9]+,RESOLUTION=(12[78][7890]x[0-9]+|[0-9]+x7[12][7890]),FRAME-RATE=[0-9]+(\.[0-9]+)?,CODECS="[^"]+"\n)(https:\/\/.+)';
+480pRE='^(\#EXT-X-STREAM-INF:PROGRAM-ID=[0-9]+,BANDWIDTH=[0-9]+,RESOLUTION=(8[45][890123]x[0-9]+|[0-9]+x4[78][7890]),FRAME-RATE=[0-9]+(\.[0-9]+)?,CODECS="[^"]+"\n)(https:\/\/.+)';
