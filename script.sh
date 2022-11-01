3c563bba381c644e4c779010a855a8a46489efb4
3ndrias <endriasargaw@gmail.com>
2022-11-01 13:28:29 +0300

    incremental backup

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# call as ./script.sh <M3U8 playlist URL>
 # this script relies upon pcregrep - one that supports multiline regular expressions
+# 1. get the playlist
+page="$(wget -O - "$1")";
+
