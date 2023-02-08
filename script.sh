36bc23685e8f47bde561426f8b28b534ca2d9708
3ndrias <enduad@gmail.com>
2023-02-08 17:23:27 +0300

    incremental backup

--- script.sh
+++ script.sh
@@ -9,6 +9,7 @@ url=`egrep -ioe '^https?:\/\/.+$' <<< "$1"`;
 page="";
 indexClass="";
 multivarChoice="";
+segmentsFile="";
 [ -z "$url" ] && echo "[error]: the calling string is not a valid http or https URL" && exit 1;
 function fetch(){
 	page=`wget -q -O - "$1"`;
@@ -32,17 +33,28 @@ case "$indexClass" in
 			do
 				echo "$i: $choice";
 				i=$((++i));
-		done <<< `egrep -ioe '\#EXT-X-STREAM-INF *: *(PROGRAM-ID=([0-9]+)|BANDWIDTH=([^,]+)|RESOLUTION=([0-9]+x[0-9]+)|CODECS="([^"]+)"|FRAME-RATE=([0-9\.]+)|,)+' <<< "$page" | sed -Ee 's/\#EXT-X-STREAM-INF *: *(PROGRAM-ID=([0-9]+)|BANDWIDTH=([^,]+)|RESOLUTION=([0-9]+x[0-9]+)|CODECS="([^"]+)"|FRAME-RATE=([0-9\.]+)|,)+/Resolution: \4, CoDecs: \5, Framerate: \6/gi'`
+		done <<< `egrep -ioe '\#EXT-X-STREAM-INF *: *(PROGRAM-ID=([0-9]+)|BANDWIDTH=([^,]+)|RESOLUTION=([0-9]+x[0-9]+)|CODECS="([^"]+)"|FRAME-RATE=([0-9\.]+)|,)+' <<< "$page" | sed -Ee 's/\#EXT-X-STREAM-INF *: *(PROGRAM-ID=([0-9]+)|BANDWIDTH=([^,]+)|RESOLUTION=([0-9]+x[0-9]+)|CODECS="([^"]+)"|FRAME-RATE=([0-9\.]+)|,)+/Resolution: \4, CoDecs: \5, Framerate: \6/gi'`;
 		read -N 1 multivarChoice;
 		while ( ! egrep -qe '^[0-9]$' <<< "$multivarChoice" )
 		do
-			echo "[inputError]: input only a single number"
+			echo "[inputError]: input only a single number";
 			read -N 1 multivarChoice;
 		done
-		var=$(egrep -ioe '\#EXT-X-STREAM-INF *: *(PROGRAM-ID=([0-9]+)|BANDWIDTH=([^,]+)|RESOLUTION=([0-9]+x[0-9]+)|CODECS="([^"]+)"|FRAME-RATE=([0-9\.]+)|,)+' <<< "$page" | sed -n "${multivarChoice}p")
+		var=$(egrep -ioe '\#EXT-X-STREAM-INF *: *(PROGRAM-ID=([0-9]+)|BANDWIDTH=([^,]+)|RESOLUTION=([0-9]+x[0-9]+)|CODECS="([^"]+)"|FRAME-RATE=([0-9\.]+)|,)+' <<< "$page" | sed -n "${multivarChoice}p");
+		selectionUrl=`pcregrep -M "${var}\n.+" <<< "$page" | tail -n 1`;
+		egrep -qie '^https?:\/\/.+$' <<< "$selectionUrl" || selectionUrl="$(sed -Ee 's/^(.+\/).+?$/\1/gi' <<< "$url")$selectionUrl";
+		segmentsFile=`wget -q -O - "$selectionUrl"`;
+		i=1;
+		[ -f temp.vid ] && echo "[input]: remove the previously incomplete temporary downloaded file 'temp.vid' ? (y|N)" && read reply && egrep -qie '^y$' <<< "$reply" && rm temp.vid;
+		[ -f temp.vid ] && echo "[exit]: re-run the script after dealing with the file named temp.vid" && exit 0;
+
+		while read segmentUrl;
+		do
+			(wget -O - >> temp.vid && echo "[info]: got segment $i") || (echo "[exit]: fatal error when downloading segment $i" && exit 1);
+			i=$((++i));
+		done <<< `egrep -ve '^\#.+' <<< $segmentsFile`;
+
 		;;
-		selectionUrl=`pcregrep -M "${var}\n.+" <<< "$page" | tail -n 1`
-		egrep -qie '^https?:\/\/.+$' <<< "$selectionUrl" || selectionUrl="$(sed -Ee 's/^(.+\/).+?$/\1/gi' <<< "$url")$selectionUrl"
 	"segments" )
 		echo  "the index file is a segments index"
 		;;
